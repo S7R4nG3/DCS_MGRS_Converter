fe6b425403b90d8a2ba3a8d807c6f537e34abaae
Dave <s7r4ng3@gmail.com>
2020-10-12 12:44:28 -0400

    updates

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-#!/usr/bin/env python3
 # By Icarus747
+# Contributors: S7R4nG3
 # Created 10/11/2020
-
-# Used for converting DCS MGRS grid coordinates to Lat/Long coordinates.
-
 import mgrs
 
+class CoordConvert:
+    def __init__(self, input_format, input_data):
+        self.mgrs = mgrs.MGRS()
 
-def main():
-    m = mgrs.MGRS()
-    # dcs = '38TLN046623'
-    dcs = input("Enter MGRS cord.")
+        lat, lon = {}, {}
+        if input_format.lower() == 'mgrs':
+            latd, lond = self.mgrs.toLatLon(input_data)
+            lat['d'],lat['m'],lat['s'] = self.mgrs.ddtodms(latd)
+            lon['d'],lon['m'],lon['s'] = self.mgrs.ddtodms(lond)
+            dms_string = "N {:.0f} {:.0f} {:.4f},".format(lat['d'],lat['m'],lat['s']) + 'E ' if lon['d'] > 0 else 'W ' + '{:.0f} {:.0f} {:.4f}'.format(abs(lon['d']),lon['m'],lon['s'])
+            print('MGRS to Decimal: {:.4f} {:.4f}'.format(latd,lond))
+            print('MGRS to DMS: {}'.format(dms_string))
+        if input_format.lower() == 'dd':
+            to_mgrs = self.mgrs.toMGRS(input_data[0],input_data[1])
+            lat['d'],lat['m'],lat['s'] = self.mgrs.ddtodms(input_data[0])
+            lon['d'],lon['m'],lon['s'] = self.mgrs.ddtodms(input_data[1])
+            dms_string = 'N {:.0f} {:.0f} {:.4f}, E 0{:.0f} {:.0f} {:.4f}'.format(lat['d'],lat['m'],lat['s'],lon['d'],lon['m'],lon['s'])
+            print('Decimal to MGRS: {}'.format(to_mgrs))
+            print('Decimal to DMS: {}'.format(dms_string))
+        if input_format.lower() == 'dms':
+            print('DMS to Decimal: {}')
+            print('DMS to MGRS: {}')
 
-    dd = m.toLatLon(dcs)
 
-    lat = m.ddtodms(dd[0])
-    Lat = round(lat[1] + lat[2] / 60, 1)
-
-    long = m.ddtodms(dd[1])
-    Long = round(long[1] + long[2] / 60, 1)
-
-    print(f"N{int(lat[0])} {Lat}")
-    print(f"E0{int(long[0])} {Long}")
-
+# def main():
+#     m = mgrs.MGRS()
+#     # dcs = '38TLN046623'
+#     dcs = input("Enter MGRS cord.")
+#     dd = m.toLatLon(dcs)
+#     lat = m.ddtodms(dd[0])
+#     Lat = round(lat[1] + lat[2] / 60, 1)
+#     long = m.ddtodms(dd[1])
+#     Long = round(long[1] + long[2] / 60, 1)
+#     print(f"N{int(lat[0])} {Lat}")
+#     print(f"E0{int(long[0])} {Long}")
 
 if __name__ == '__main__':
-    main()
-    # TODO: Error handeling for incorect MGRS grid.
+    # main()
+    CoordConvert('mgrs', '15TWG0000049776')
+    # CoordConvert('dd', '')
+    # CoordConvert('dms', '')
