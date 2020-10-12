e84f0b485cdbeeb79d7dac984b97f4576ac8f18e
Dave <s7r4ng3@gmail.com>
2020-10-12 15:08:30 -0400

    longitude cardinal swap

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ class CoordConvert:
             to_mgrs = self.mgrs.toMGRS(input_data[0],input_data[1])
             lat['d'],lat['m'],lat['s'] = self.mgrs.ddtodms(input_data[0])
             lon['d'],lon['m'],lon['s'] = self.mgrs.ddtodms(input_data[1])
-            dms_string = 'N {:.0f} {:.0f} {:.4f}, E 0{:.0f} {:.0f} {:.4f}'.format(lat['d'],lat['m'],lat['s'],abs(lon['d']),lon['m'],lon['s'])
+            dms_string = ('N ' if lat['d'] > 0 else 'S ') + '{:.0f} {:.0f} {:.4f}, '.format(lat['d'],lat['m'],lat['s']) + ('E ' if lon['d'] > 0 else 'W ') + '{:.0f} {:.0f} {:.4f}'.format(abs(lon['d']),lon['m'],lon['s'])
             print('Decimal to MGRS: {}'.format(to_mgrs))
             print('Decimal to DMS: {}'.format(dms_string))
         if input_format.lower() == 'dms':
             print('\nDMS Input...', input_data)
             lat_float, lon_float = tuple(float(i) for i in input_data[0]), tuple(float(j) for j in input_data[1])
             lat_string = '{:.0f}{:.0f}{:.2f}'.format(lat_float[0],lat_float[1],lat_float[2]) + ( 'N' if lat_float[0] > 0 else 'S' )
-            lon_string = '{:.0f}{:.0f}{:.2f}'.format(lon_float[0],lon_float[1],lon_float[2]) + ( 'E' if lon_float[0] > 0 else 'W' )
+            lon_string = '{:.0f}{:.0f}{:.2f}'.format(lon_float[0],lon_float[1],lon_float[2]) + ( 'W' if lon_float[0] > 0 else 'E' )
             lat_to_dd = self.mgrs.dmstodd(lat_string)
             lon_to_dd = self.mgrs.dmstodd(lon_string)
             to_mgrs = self.mgrs.toMGRS(lat_to_dd, lon_to_dd)
@@ -36,6 +36,6 @@ class CoordConvert:
             print('DMS to MGRS: {}'.format(to_mgrs))
 
 if __name__ == '__main__':
-    CoordConvert('mgrs', '44SMD4521498457')
+    CoordConvert('mgrs', '17SNU5478598457')
     CoordConvert('dd', (35.227672, -80.397949))
-    CoordConvert('dms', [(35,13,39.6192),(80,23,52.6164)])
+    CoordConvert('dms', [(35,13,39.6192),(-80,23,52.6516)])
