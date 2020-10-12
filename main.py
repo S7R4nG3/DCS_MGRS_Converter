fdf134c5c36bdf4a239d077e96859b414707bcc3
Dave <s7r4ng3@gmail.com>
2020-10-12 12:54:12 -0400

    fixed dms string formatting

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ class CoordConvert:
             latd, lond = self.mgrs.toLatLon(input_data)
             lat['d'],lat['m'],lat['s'] = self.mgrs.ddtodms(latd)
             lon['d'],lon['m'],lon['s'] = self.mgrs.ddtodms(lond)
-            dms_string = "N {:.0f} {:.0f} {:.4f},".format(lat['d'],lat['m'],lat['s']) + 'E ' if lon['d'] > 0 else 'W ' + '{:.0f} {:.0f} {:.4f}'.format(abs(lon['d']),lon['m'],lon['s'])
-            print('MGRS to Decimal: {:.4f} {:.4f}'.format(latd,lond))
+            dms_string = ('N ' if lat['d'] > 0 else 'S ') + '{:.0f} {:.0f} {:.4f}, '.format(lat['d'],lat['m'],lat['s']) + ('E ' if lon['d'] > 0 else 'W ') + '{:.0f} {:.0f} {:.4f}'.format(abs(lon['d']),lon['m'],lon['s'])
+            print('\nMGRS to Decimal: {:.4f} {:.4f}'.format(latd,lond))
             print('MGRS to DMS: {}'.format(dms_string))
         if input_format.lower() == 'dd':
             to_mgrs = self.mgrs.toMGRS(input_data[0],input_data[1])
             lat['d'],lat['m'],lat['s'] = self.mgrs.ddtodms(input_data[0])
             lon['d'],lon['m'],lon['s'] = self.mgrs.ddtodms(input_data[1])
-            dms_string = 'N {:.0f} {:.0f} {:.4f}, E 0{:.0f} {:.0f} {:.4f}'.format(lat['d'],lat['m'],lat['s'],lon['d'],lon['m'],lon['s'])
-            print('Decimal to MGRS: {}'.format(to_mgrs))
+            dms_string = 'N {:.0f} {:.0f} {:.4f}, E 0{:.0f} {:.0f} {:.4f}'.format(lat['d'],lat['m'],lat['s'],abs(lon['d']),lon['m'],lon['s'])
+            print('\nDecimal to MGRS: {}'.format(to_mgrs))
             print('Decimal to DMS: {}'.format(dms_string))
         if input_format.lower() == 'dms':
-            print('DMS to Decimal: {}')
+            print('\nDMS to Decimal: {}')
             print('DMS to MGRS: {}')
 
 
@@ -42,5 +42,5 @@ class CoordConvert:
 if __name__ == '__main__':
     # main()
     CoordConvert('mgrs', '15TWG0000049776')
-    # CoordConvert('dd', '')
+    CoordConvert('dd', (42.0, -93.0))
     # CoordConvert('dms', '')
